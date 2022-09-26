f620e2758e07a15777570b33ee299c3a9dc95860
Jon <jtan5@jaguarlandrover.com>
2022-09-26 15:03:42 +0100

    Included global

--- source/access_func.py
+++ source/access_func.py
@@ -7,11 +7,12 @@ import os
 def import_products():
     
     choice_list = []#['Drinks', 'Cold food', 'Hot food']
-    drinks = []#['Still water 500ml', 'Sparking water 500ml', 'Fanta Zero 350ml',
+    #drinks = []#['Still water 500ml', 'Sparking water 500ml', 'Fanta Zero 350ml',
             #'Coke Zero 350ml', 'Don Perignon in paper bag 350ml']
-    cold_food = []#['Vegan sandwich', 'Chicken sandwich', 'Cheese block']
-    hot_food = []#["Vegan pie", "Meat pie",
+    #cold_food = []#['Vegan sandwich', 'Chicken sandwich', 'Cheese block']
+    #hot_food = []#["Vegan pie", "Meat pie",
                 #"Jacket potato with beans", "Fish and chips"]
+    category_new = []
     
     current_path = os.getcwd()
     parent_path = os.path.dirname(current_path)
@@ -26,16 +27,25 @@ def import_products():
     for index, filename in enumerate(os.listdir(products_path)):
         print("printing filename",index,filename)
         #starting with an empty list, populating choicelist based on filenames, stripping out .txt
-        choice_list.append(filename.strip(".txt"))
-        print(choice_list)
-        filename.strip(".txt") = []
+        clean_name = filename.strip(".txt")
+        choice_list.append(clean_name)
+        globals()[filename.strip(".txt")] = []
+        #exec("%s=%d" % (filename,5000))
+
+        print(f"This is the choise list :{choice_list}")
+        #category_new[index] = filename.strip(".txt")
         (os.path.join(products_path, filename))
         with open(os.path.join(products_path, filename), 'r') as file:
             #text = file.read()
             #print(text)
-            for line_index, line_value in enumerate(file.readlines()):
-                clean_name = line_value.rstrip()
-                print(f"For line_index {line_index}:{clean_name}")
+            globals()[filename.strip(".txt")] = file.read().splitlines(0)
+            # print(f"Initial filename contents: {filename}")
+            # for line_index, line_value in enumerate(file.readlines()):
+            #     clean_name = line_value.rstrip()
+            #     print(f"clean_name: {clean_name}")
+            #     filename[line_index] = clean_name
+            #     print(f"For line_index {line_index}:{clean_name}")
+            print(f"Final filename contents of file: {clean_name}: {globals()[clean_name]}")
             
     
     
